de914cce7b55979e538eb2bfc0c847d933956f38
Pavel <lebedev@haulmont.com>
2018-04-26 19:26:45 +0400

    docommit

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -36,6 +36,8 @@ create table SALESCAFE_ORDER (
     TIME_OF_ORDER timestamp,
     ALL_COST integer,
     COMMENT_ varchar(255),
+    WORK_DAY_ID uuid,
+    TYPE_OF_CUSTOMER integer,
     --
     primary key (ID)
 )^
@@ -52,11 +54,30 @@ create table SALESCAFE_PRODUCT (
     DELETED_BY varchar(50),
     --
     PRODUCT_NAME varchar(255),
+    WEIGHT integer,
     PRODUCT_PRICE integer,
-    IS_NOT_AVAILABLE boolean,
+    IS_AVAILABLE boolean,
     RESIDUE integer,
     PRODUCT_TYPE integer,
     --
     primary key (ID)
 )^
 -- end SALESCAFE_PRODUCT
+-- begin SALESCAFE_WORK_DAY
+create table SALESCAFE_WORK_DAY (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    TOTAL_PROFIT integer,
+    DAY_OF_WEEK integer,
+    WORK_DATE date,
+    --
+    primary key (ID)
+)^
+-- end SALESCAFE_WORK_DAY
